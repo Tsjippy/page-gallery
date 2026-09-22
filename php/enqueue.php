ffcdb5c16f238d8286aca05474e9c22588d7a997
Tsjippy <ewaldha@hotmail.com>
2026-09-22 06:14:25 +0200

    changed: js refactor

--- php/enqueue.php
+++ php/enqueue.php
@@ -7,7 +7,11 @@ use TSJIPPY;
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\loadAssets');
 function loadAssets()
 {
-    wp_register_script_module('@tsjippy/page_gallery_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/page_gallery' . TSJIPPY\JSEXTENSION), array('@tsjippy/formsubmit_script'), PLUGINVERSION);
+    $deps   = SCRIPT_DEBUG ? [  
+        '@tsjippy/form_submit_functions'
+    ] :
+    [];
+    wp_register_script_module('@tsjippy/page_gallery_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/page_gallery' . TSJIPPY\JSEXTENSION), $deps, PLUGINVERSION);
 
     wp_register_style('tsjippy_page_gallery_style', TSJIPPY\pathToUrl(PLUGINPATH . 'css/page_gallery.min.css'), array(), PLUGINVERSION);
 }
