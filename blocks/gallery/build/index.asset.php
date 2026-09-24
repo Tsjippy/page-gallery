303c2eafa0dadc688cef1508539f8a24e9b3739a
Tsjippy <ewaldha@hotmail.com>
2026-09-24 07:40:21 +0200

    fixed: js variables

--- blocks/gallery/build/index.asset.php
+++ blocks/gallery/build/index.asset.php
@@ -1,1 +1,15 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2fba2f692d7fc6b3a24f');
+<?php return array(
+	'dependencies' => array(
+		'react-jsx-runtime',
+		'wp-api-fetch',
+		'wp-block-editor',
+		'wp-blocks',
+		'wp-components',
+		'wp-core-data',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+		'wp-server-side-render'
+	),
+	'version' => '2fba2f692d7fc6b3a24f'
+);
--- php/enqueue.php
+++ php/enqueue.php
@@ -5,12 +5,17 @@ namespace TSJIPPY\PAGEGALLERY;
 use TSJIPPY;
 
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\loadAssets');
+/**
+ * Registeres the CSS and JS
+ */
 function loadAssets()
 {
     $deps   = SCRIPT_DEBUG ? [  
         '@tsjippy/form_submit_functions'
     ] :
     [];
+
+    $deps[] = "@tsjippy/nonce_script";
     wp_register_script_module('@tsjippy/page_gallery_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/page_gallery' . TSJIPPY\JSEXTENSION), $deps, PLUGINVERSION);
 
     wp_register_style('tsjippy_page_gallery_style', TSJIPPY\pathToUrl(PLUGINPATH . 'css/page_gallery.min.css'), array(), PLUGINVERSION);
